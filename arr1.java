dd66824ad121aefa07269526d5b3ea38e694ff87
omprakash344 <omprakashpr2022@gmail.com>
2023-11-07 09:02:35 +0530

    firstcommit By om

---
+++ arr1.java
@@ -0,0 +1,14 @@
+public class arr1 {
+    public static void main(String[] args) {
+        int nums[]=new int[4];
+        nums[0]=12;
+        nums[1]=23;
+         nums[2]=1233;
+         System.out.println(nums);  
+         for(int i=0;i<=3;i++){
+            System.out.println(nums[i]);
+         }
+        
+    }
+    
+}
